791cbe5d27130ac4a4aebb53ee9464e9769f5205
Juan Vargas <juandeveloper19@gmail.com>
2022-05-08 05:28:15 -0500

    The global state of the application and the reducer were created to work the actions

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,18 +1,30 @@
+import { useReducer } from 'react';
+
 // Containers
 import AddUsers from './AddUsers';
 import Modals from './Modals';
 import UserList from './UserList';
 
+// Custom Hooks
+import { useUserReducer } from '../hooks/useUserReducer';
+
+// Context
+import { UserContext } from '../context/userContext';
+
 const Main = () => {
+  const [users, dispatch] = useReducer(useUserReducer, []);
+
   return (
-    <div className='main'>
-      <div className='main-content'>
-        <AddUsers>
-          <Modals />
-          <UserList />
-        </AddUsers>
+    <UserContext.Provider value={{ users, dispatch }}>
+      <div className='main'>
+        <div className='main-content'>
+          <AddUsers>
+            <Modals />
+            <UserList />
+          </AddUsers>
+        </div>
       </div>
-    </div>
+    </UserContext.Provider>
   );
 };
 
---
+++ src/hooks/useUserReducer.js
@@ -0,0 +1,6 @@
+export const useUserReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
