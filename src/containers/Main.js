ab3aa3201136b146bc7b1c9613d1e3016073d3cd
Juan Vargas <juandeveloper19@gmail.com>
2022-05-08 05:46:50 -0500

    Recovering users from local storage

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -11,8 +11,11 @@ import { useUserReducer } from '../hooks/useUserReducer';
 // Context
 import { UserContext } from '../context/userContext';
 
+// Helpers
+import { init } from '../helpers/init';
+
 const Main = () => {
-  const [users, dispatch] = useReducer(useUserReducer, []);
+  const [users, dispatch] = useReducer(useUserReducer, [], init);
 
   // Save users to LocalStorage
   useEffect(() => localStorage.setItem('users', JSON.stringify(users)), [users]);
